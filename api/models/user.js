42cba228b43df447c6b2034c4eb91db81b75323a
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-04-27 06:50:29 +0100

    secuirty update

--- api/models/user.js
+++ api/models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 
 const userSchema = mongoose.Schema({
     email: {
@@ -12,7 +15,47 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
         minlength: 6
-    }
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+userSchema.statics.findByCredentials = function(email, password){
+    var User = this;
+    return User.findOne({email}).then(user => {
+        if(!user){ return Promise.reject(); }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if(res){ resolve(user); }
+                else { reject(); }
+            });
+        });
+    });
+};
+
+userSchema.methods.generateAuthToken = function(){
+    const user = this;
+    const access = 'auth';
+    const token = jwt.sign( {_id: user._id.toHexString(), access},
+        process.env.JWT_KEY).toString();
+    
+    user.tokens = user.tokens.concat({access, token});
+    return user.save().then(() => { return token });
+};
+
+//removes token
+userSchema.methods.removeToken = function(){
+    var user = this;
+    return user.update({ $pull: {tokens: {token}} });
+};
 
 module.exports = mongoose.model('user', userSchema);
--- api/routes/user.js
+++ api/routes/user.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
+//create new user
 router.post('/signup', (req, res, next) => {
     User.find({email: req.body.email})
     .exec().then(user => {
@@ -32,5 +35,65 @@ router.post('/signup', (req, res, next) => {
         }//else
     });
 });//.post
+
+/*
+//user login
+router.post('/login', (req, res, next) => {
+    User.find({email: req.body.email})
+    .exec().then(user => {
+        if(user.length < 1){
+            return res.status(401).json({message: 'failed to find email'});
+        }
+        
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+            if(err){
+                return res.status(401).json({message: 'password wrong'});
+            }
+            if(result){
+                const token = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                }, process.env.JWT_KEY,
+                {expiresIn: '1h'});
+                return res.status(200).json({
+                    message: 'Login successful',
+                    token: token
+                });
+            }
+            res.status(401).json({message: 'AUTH FAILED'});
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+*/
+
+//setup auto delete of token on logout
+//user login2
+router.post('/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then(user => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch(err => { res.status(400).json({error: 'login fail'}); });    
+});
+
+//delete user
+router.delete('/:token', (req, res, next) => {
+    req.delete.removeToken(req.token).then(() => {
+        res.status(200).json({message: 'Logout successful'});
+    }, () => { res.status(400).send(); });
+});
+
+
+router.get('/me', (req, res) => {
+    res
+});
+
 
 module.exports = router;

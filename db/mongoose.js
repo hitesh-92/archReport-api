a2f760863fa52d30aa5eb80f99e596561bc95279
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-06-27 18:22:25 +0100

    api connected to MongoAtlas and working

--- db/mongoose.js
+++ db/mongoose.js
@@ -6,8 +6,9 @@ mongoose.Promise = global.Promise;
 // mongoose.connect(process.env.MONGODB_URI);
 
 //Connect to mongoAtlas
-mongoose.connect(
-    `mongodb+srv://hitesh:${process.env.MONGOATLAS}@archr-s7d4t.gcp.mongodb.net/test?retryWrites=true`
-);
+// mongoose.connect(`mongodb+srv://hitesh:W8fSeRuW7M2BKuBL@archr-s7d4t.gcp.mongodb.net/test?retryWrites=true`);
+
+mongoose.connect(`mongodb://hitesh:W8fSeRuW7M2BKuBL@archr-shard-00-00-s7d4t.gcp.mongodb.net:27017,archr-shard-00-01-s7d4t.gcp.mongodb.net:27017,archr-shard-00-02-s7d4t.gcp.mongodb.net:27017/test?ssl=true&replicaSet=archR-shard-0&authSource=admin&retryWrites=true`);
 
 module.exports = {mongoose};
+
